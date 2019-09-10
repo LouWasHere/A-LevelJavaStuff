d314e6b5b3604269b023909470f4b21b97d42217
Louis Joseph Bishop <louis.bishop@live.co.uk>
2019-09-10 11:31:32 +0100

    do dat jazz

---
+++ Java/TheQuiz/src/thequiz/TheQuiz.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thequiz;
+import java.io.*;
+/**
+ *
+ * @author l-bishop
+ */
+public class TheQuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String myGuess = null;
+        BufferedReader x = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("When was the World Wide Web created?");
+        System.out.println("A - 1990");
+        System.out.println("B - 1991");
+        System.out.println("C - 1992");
+        System.out.println("D - 1993");
+        
+    }
+    
+}
