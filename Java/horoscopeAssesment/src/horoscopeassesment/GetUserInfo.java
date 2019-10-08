f09e8136d78a2cecf548e389baf8641cbbe655a0
Louis Joseph Bishop <louis.bishop@live.co.uk>
2019-10-08 12:55:40 +0100

    mwahahahaha

---
+++ Java/horoscopeAssesment/src/horoscopeassesment/GetUserInfo.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horoscopeassesment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.*;
+
+/**
+ *
+ * @author l-bishop
+ */
+public class GetUserInfo 
+{
+    public static boolean isNumeric(String strNum) 
+    {
+        try 
+        {
+            double d = Double.parseDouble(strNum);
+        } 
+        catch (NumberFormatException | NullPointerException nfe)
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    public static void getName()
+    {
+        String inputStringName = null;
+        boolean loop = true;
+        while(loop == true)
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("What is your name?");
+            try
+            {
+                inputStringName = br.readLine();
+            }
+            catch (IOException e)
+            {
+                System.out.println("Error: " + e);
+            }
+            if("".equals(inputStringName))
+            {
+                System.out.println("That wasn't a name, silly!");
+            }
+            else
+            {
+                loop = false;
+            }
+        }
+        getDateOfBirth(inputStringName);
+    }
+    public static void getDateOfBirth(String name)
+    {
+        LocalDate dateOfBirth = LocalDate.now();
+        boolean loop = true;
+        while(loop == true)
+        {
+            String inputStringYear = null;
+            String inputStringMonth = null;
+            String inputStringDay = null;
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("What year were you born in?");
+            try
+            {
+                inputStringYear = br.readLine();
+            }
+            catch (IOException e)
+            {
+                System.out.println("Error: " + e);
+            }
+            if(isNumeric(inputStringYear) == false)
+            {
+                System.out.println("Doofus. That wasn't a valid number, was it?");
+            }
+            else
+            {
+                System.out.println("What month were you born in? (1 to 12)");
+                try
+                {
+                    inputStringMonth = br.readLine();
+                }
+                catch (IOException e)
+                {
+                    System.out.println("Error: " + e);
+                }
+                double d = Double.parseDouble(inputStringMonth);
+                if(isNumeric(inputStringMonth)==false || d>12 || d<1)
+                {
+                System.out.println("Doofus. That wasn't a valid number, was it?");
+                }
+                else
+                {
+                    System.out.println("What day were you born on? (1 to 31)");
+                    try
+                    {
+                        inputStringDay = br.readLine();
+                    }
+                    catch (IOException e)
+                    {
+                        System.out.println("Error: " + e);
+                    }
+                    d = Double.parseDouble(inputStringDay);
+                    if(isNumeric(inputStringDay)==false || d>31 || d<1)
+                    {
+                    System.out.println("Doofus. That wasn't a valid number, was it?");
+                    }
+                    else
+                    {
+                        int year = Integer.parseInt(inputStringYear);
+                        int month = Integer.parseInt(inputStringMonth);
+                        int day = Integer.parseInt(inputStringDay);
+                        dateOfBirth = LocalDate.of(year, month, day);
+                        loop = false;
+                    }
+                }
+            }
+        }
+        System.out.println("Ah, so you were born on " + dateOfBirth.toString());
+        DisplayHoroscope dispHoro = new DisplayHoroscope();
+        dispHoro.displayHoroscope(name, dateOfBirth);
+    }
+}
---
+++ Java/horoscopeAssesment/src/horoscopeassesment/HoroscopeAssesment.java
@@ -0,0 +1,56 @@
+package horoscopeassesment;
+import java.time.*;
+import java.io.*;
+import java.util.*;
+
+public class HoroscopeAssesment {
+
+    public static String startMenu()
+    {
+        boolean loop = true;
+        while(loop == true)
+        {
+            String inputString = null;
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("1 – Display Horoscope\n2 - Exit");
+            try
+            {
+                inputString = br.readLine();
+            }
+            catch (IOException e)
+            {
+                System.out.println("Error: " + e);
+            }
+            if(inputString.equals("1")||inputString.equals("2"))
+            {
+                loop = false;
+                return inputString;
+            }
+            else
+            {
+                System.out.println("Please enter a valid option.\n");
+            }
+        }
+        return null;
+        
+    }
+    
+    public static void main(String[] args) 
+    {
+        boolean end = false;
+        while(end == false)
+        {
+            String menuSelection = startMenu();
+            if("2".equals(menuSelection))
+            {
+                end = true;
+            }
+            else if("1".equals(menuSelection))
+            {
+                GetUserInfo getUserInfo = new GetUserInfo();
+                getUserInfo.getName();
+            }
+        }
+    }
+    
+}
