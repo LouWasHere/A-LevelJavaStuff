824cfdba227d968b116bbf1cf2317e6f1f4197d2
Louis Joseph Bishop <louis.bishop@live.co.uk>
2019-11-12 11:28:38 +0000

    t6tyt

---
+++ Java/HashingPotatoes/src/hashingpotatoes/HashingPotatoes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashingpotatoes;
+
+import java.util.*;
+
+public class HashingPotatoes
+{
+
+    public static void main(String[] args) 
+    {
+        ArrayList<Integer> values = new ArrayList<>();
+        values.add(78);
+        values.add(55);
+        values.add(34);
+        values.add(19);
+        values.add(29);
+        
+        ArrayList<String>[][] hashTables = new ArrayList[10][10];
+       // hashTables[0][0] = new ArrayList<>();
+        hashTables[0][0].add("test");
+    }
+    
+}
