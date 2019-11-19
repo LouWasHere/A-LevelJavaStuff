71668dbbfbbbf9ef58333a0d6ac7b8099981bad6
Louis Joseph Bishop <louis.bishop@live.co.uk>
2019-11-19 15:36:47 +0000

    add

---
+++ Java/BinaryTreePractice/src/binarytreepractice/BinaryTree.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarytreepractice;
+
+/**
+ *
+ * @author l-bishop
+ */
+public class BinaryTree
+{
+    Node placeholder = new Node("Placeholder", null, null, null);
+    
+    public void declareRoot(String value)
+    {
+        Node root = new Node("Jim", placeholder, placeholder, placeholder);
+    }
+    
+    public void newNode(String value, String direction, Node parent)
+    {
+        
+    }
+    
+}
---
+++ Java/BinaryTreePractice/src/binarytreepractice/Node.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarytreepractice;
+
+class Node
+{
+    String desc;
+    Node left;
+    Node right;
+    Node parent;
+    
+    public Node(String desc, Node left, Node right, Node parent) 
+    {
+       this.desc = desc;
+       this.left = left;
+       this.right = right;
+       this.parent = parent;
+    }
+}
